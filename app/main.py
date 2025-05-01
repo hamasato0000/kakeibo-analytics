43dd121e2b93e41eacce735697ad2562f8d99f26
Masato Hamasaki <hamasato48@gmail.com>
2025-05-02 00:02:08 +0900

    add: アプリケーションロゴ表示を追加

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
+import os
 import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
+
+APP_LOGO = os.environ["APP_LOGO"]
 
 pg = st.navigation([
     st.Page("home.py", title="ホーム", icon="🏠️"),
     st.Page("balance.py", title="収支分析", icon="📊")
 ])
 pg.run()
+
+st.logo(image=APP_LOGO, size="large")
