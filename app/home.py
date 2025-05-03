73c5febb318730aea2c0c5ec7cd3da49ba1239e4
Masato Hamasaki <hamasato48@gmail.com>
2025-05-03 15:32:09 +0900

    update: ユーザー情報を常に表示するように修正

--- app/home.py
+++ app/home.py
@@ -7,4 +7,4 @@ if st.button("Log in"):
 if st.user.is_logged_in:
     if st.button("Log out"):
         st.logout()
-    st.write(f"Hello, {st.user.name}!")
+st.write(f"Hello, {st.user.to_dict()}!")
