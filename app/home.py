dbd7cef126f46368b3efee65e66a80a3b3c99bf8
Masato Hamasaki <hamasato48@gmail.com>
2025-05-05 09:44:49 +0900

    update: サイドバーにユーザー名とログアウトボタンが表示されるように修正

--- app/home.py
+++ app/home.py
@@ -4,7 +4,8 @@ st.title("家計簿アプリ")
 
 if st.button("Log in"):
     st.login("auth0")
-if st.user.is_logged_in:
-    if st.button("Log out"):
-        st.logout()
 st.write(f"Hello, {st.user.to_dict()}!")
+
+if st.user.is_logged_in:
+    st.sidebar.write(st.user.name)
+    st.sidebar.button("Log out", on_click=st.logout, icon=":material/logout:")
