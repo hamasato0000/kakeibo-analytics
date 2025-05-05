23f4d723b3b670f352344ff40ac2127fa9193ecf
Masato Hamasaki <hamasato48@gmail.com>
2025-05-05 10:10:03 +0900

    uupdate: ログアウトしている場合はサイドバーを非表示

--- app/home.py
+++ app/home.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.title("💴 お金の管理を最適化しよう")
 
-if st.button("Log in"):
-    st.login("auth0")
-st.write(f"Hello, {st.user.to_dict()}!")
-
-if st.user.is_logged_in:
-    st.sidebar.write(st.user.name)
-    st.sidebar.button("Log out", on_click=st.logout, icon=":material/logout:")
+if not st.user.is_logged_in:
+    st.markdown("#### まずはログイン")
+    if st.button("ログイン", icon=":material/login:"):
+        st.login("auth0")
+else:
+    st.sidebar.write(f"ログイン中：{st.user.name}")
+    st.sidebar.button("ログアウト", on_click=st.logout, icon=":material/logout:")
