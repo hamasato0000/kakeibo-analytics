88651b9008a19a236f162701968e7b83ccfcbb12
Masato Hamasaki <hamasato48@gmail.com>
2025-05-14 23:41:21 +0900

    add: チャット入力機能を仮実装

--- app/chat.py
+++ app/chat.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import numpy as np
 
 with st.chat_message("user"):
     st.write("Hello 👋")
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
